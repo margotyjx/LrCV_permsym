301b53462381136314edd7d6693f7e4a6f1dae3d
Jiaxin Yuan <jiaxinyuan@Jiaxins-mac.local>
2025-04-16 11:16:49 -0400

    update cv learning

--- CV_learning/dMap_based/main_DDnet.py
+++ CV_learning/dMap_based/main_DDnet.py
@@ -1,40 +1,21 @@
 import os
-import sys
 import time
-
+import argparse
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.init as init
-# from torchvision import datasets
-# from torchvision import transforms as T
-import torch.distributed as dist
 import torch.optim as optim
-# from torch.nn.parallel import DistributedDataParallel as DDP
-# from torch.utils.data.distributed import DistributedSampler
-from torch.utils.data import TensorDataset
-from torch.utils.data import DataLoader
-from torch.nn import functional as F
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from torch.utils.data import TensorDataset, DataLoader
 import matplotlib.pyplot as plt
 
 import utils_LJ7, utils, models, utils_LJ8
 
-"""
-This file contains the code to train diffusion net
-"""
 
 class AverageMeter(object):
-    """Computes and stores the average and current value"""
     def __init__(self):
         self.reset()
-
     def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
+        self.val = self.avg = self.sum = self.count = 0
     def update(self, val, n=1):
         self.val = val
         self.sum += val * n
@@ -42,89 +23,79 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
 
-def main_DDnet():
-    
+def main(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
-    example = "LJ7"
-    generate = True
-    knn = 
-    epsilon = 1.0
-    
+    example = args.example
+    read_knn = args.knn
+    epsilon = args.epsilon
+    hidden = [int(h) for h in args.hidden.split(',')]
+    epochs = args.epochs
+
     output_folder = f"../{example}/model_output/"
     data_folder = f"../{example}/data/"
-    
-    import re
-    sigma = 0.02
-    fname = f'../{example}/data/ConfBins_data_CrdNum.csv'
-    
+    os.makedirs(output_folder, exist_ok=True)
 
-    data = np.loadtxt(fname,delimiter=",")
-
-    # ======== transformed data ======== 
+    data = np.loadtxt(f"{data_folder}/ConfBins_data_CrdNum.csv", delimiter=",")
+    N = data.shape[0]
     train_data = torch.from_numpy(data).to(torch.float32)
 
-    target = np.loadtxt(f"../{example}/data/{example}_eigens_knn{knn}_eps{epsilon}_CrdNum.csv", delimiter=',').astype(np.float32)
+    knn = int(read_knn * N) if read_knn < 1. else int(read_knn)
+    target = np.loadtxt(f"{data_folder}/{example}_eigens_knn{knn}_eps{epsilon}_CrdNum.csv", delimiter=",").astype(np.float32)
+    target_data = torch.from_numpy(target)
 
-    target_data = torch.from_numpy(target)    
-    
-    train_ds = TensorDataset(train_data,target_data)
-    batch_size = int(len(train_data)/125)
-    train_loader = DataLoader(dataset=train_ds,
-                          batch_size=128,shuffle=True) 
-    
+    train_ds = TensorDataset(train_data, target_data)
+    train_loader = DataLoader(dataset=train_ds, batch_size=128, shuffle=True)
+
     z_dim = target_data.shape[1]
-    print('target dimension: ', z_dim)
-    # hidden = [45,45]
-    hidden = [45,30,25]
-    model = models.DDnet(train_data.shape[1],hidden,z_dim,activation = nn.ELU())
-    model = model.to(device)
-    
+    print('Target dimension:', z_dim)
+
+    model = models.DDnet(train_data.shape[1], hidden, z_dim, activation=nn.ELU()).to(device)
+
     optimizer = optim.Adam(model.parameters(), lr=5e-3)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=500, gamma=0.9)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=500, gamma=0.9)
     loss_fn = nn.L1Loss()
-    # loss_fn = nn.MSELoss()
-    
-    for epoch in range(4000):
+
+    for epoch in range(epochs):
         model.train()
         i = 0
-        for x,psi in train_loader:
+        for x, psi in train_loader:
             i += 1
             batch_time = AverageMeter()
             losses = AverageMeter()
             end = time.time()
+
             x = x.to(device)
             psi = psi.to(device)
 
             eigen = model(x)
-            loss = loss_fn(eigen,psi)
+            loss = loss_fn(eigen, psi)
 
-            # compute gradient and update
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             scheduler.step()
 
-        if epoch%50 == 0:
-
+        if epoch % 50 == 0:
             reduced_loss = loss.data
             losses.update(float(reduced_loss), x.size(0))
-            # torch.cuda.synchronize()
-            batch_time.update((time.time() - end)/15)
-            end = time.time()
+            batch_time.update((time.time() - end) / 15)
+            print(f"Epoch: [{epoch}][{i}/{len(train_loader)}]\t"
+                  f"Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t"
+                  f"Loss {losses.val:.6f} ({losses.avg:.6f})")
 
-            print('Epoch: [{0}][{1}/{2}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Loss {loss.val:.6f} ({loss.avg:.6f})\t'.format(
-                        epoch, i, len(train_loader),
-                        batch_time=batch_time,
-                        loss=losses))
             torch.save(model, output_folder + f"{example}_DDnet_knn{knn}_eps{epsilon}_BinsConf_CrdNum.pt")
 
     torch.save(model, output_folder + f"{example}_DDnet_knn{knn}_eps{epsilon}_BinsConf_CrdNum.pt")
-    
-    
-        
+
+
 if __name__ == '__main__':
-    
-    main_DDnet()        
+    parser = argparse.ArgumentParser(description="Train Diffusion Net (DDNet) for LJ system")
+    parser.add_argument("--example", type=str, choices=["LJ7", "LJ8"], default="LJ7", help="System type")
+    parser.add_argument("--knn", type=float, default=0.25, 
+                        help="KNN fraction or absolute value. If less than 1, interpreted as ratio to the total number.")
+    parser.add_argument("--epsilon", type=float, default=1.0, help="Epsilon for diffusion map")
+    parser.add_argument("--hidden", type=str, default="45,30,25", help="Comma-separated hidden layer sizes")
+    parser.add_argument("--epochs", type=int, default=4000, help="Number of training epochs")
+    args = parser.parse_args()
+    main(args)
